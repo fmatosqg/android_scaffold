a3e1670d748bfddbad89f5df89b05c456a1b4f19
Fabio de Matos <fmatosqg@gmail.com>
2019-03-14 19:14:39 +1100

    Small refactor

--- app/src/main/java/net/amazingdomain/sample/myapplication/ui/landing/LandingActivity.kt
+++ app/src/main/java/net/amazingdomain/sample/myapplication/ui/landing/LandingActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.landing_toolbar.*
 import net.amazingdomain.sample.myapplication.R
 import net.amazingdomain.sample.myapplication.databinding.ActivityMainBinding
 
