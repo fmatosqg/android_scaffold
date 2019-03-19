af27df99e33eb2e277eade2fa02579c2c0a503a1
Fabio de Matos <fmatosqg@gmail.com>
2019-03-19 17:35:10 +1100

    Configure r8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
 
             // TODO replace by proper key signing for official releases
